51c86b709c2ad9c6a78e50b35ab5687ea4cb8ec8
arkajyotibose003 <arkajyotibose003@gmail.com>
2026-02-05 11:59:08 +0530

    Camera fix try 3

--- server.js
+++ server.js
@@ -8,59 +8,74 @@ const port = process.env.PORT || 10000;
 
 app.use(express.static("assests"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontpage.html"));
-});
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "frontpage.html"))
+);
+app.get("/page1", (req, res) =>
+  res.sendFile(path.join(__dirname, "index.html"))
+);
+app.get("/page2", (req, res) =>
+  res.sendFile(path.join(__dirname, "index2.html"))
+);
+app.get("/page3", (req, res) =>
+  res.sendFile(path.join(__dirname, "index3.html"))
+);
 
-app.get("/page1", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/page2", (req, res) => {
-  res.sendFile(path.join(__dirname, "index2.html"));
-});
-
-app.get("/page3", (req, res) => {
-  res.sendFile(path.join(__dirname, "index3.html"));
-});
-
-// ---- SOCKET SERVER ----
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
+const io = new Server(server, { cors: { origin: "*" } });
 
-let userCount = 0;
+// ---------------- ROOMS ----------------
+const ROOM_ID = "watch-room";
 
 io.on("connection", socket => {
-  userCount++;
+  const room = io.sockets.adapter.rooms.get(ROOM_ID);
+  const usersInRoom = room ? room.size : 0;
+
+  if (usersInRoom >= 2) {
+    socket.emit("room-full");
+    socket.disconnect();
+    return;
+  }
+
+  socket.join(ROOM_ID);
   console.log("User connected:", socket.id);
 
-  socket.emit("user-count", userCount);
+  io.to(ROOM_ID).emit("user-count", usersInRoom + 1);
 
-  socket.on("offer", data => {
-    socket.broadcast.emit("offer", data);
-  });
+  // -------- WebRTC signaling (ROOM BASED) --------
+  socket.on("offer", data =>
+    socket.to(ROOM_ID).emit("offer", data)
+  );
 
-  socket.on("answer", data => {
-    socket.broadcast.emit("answer", data);
-  });
+  socket.on("answer", data =>
+    socket.to(ROOM_ID).emit("answer", data)
+  );
 
-  socket.on("ice-candidate", candidate => {
-    socket.broadcast.emit("ice-candidate", candidate);
-  });
+  socket.on("ice-candidate", candidate =>
+    socket.to(ROOM_ID).emit("ice-candidate", candidate)
+  );
 
-  // 🎬 MOVIE SYNC
-  socket.on("movie-play", () => socket.broadcast.emit("movie-play"));
-  socket.on("movie-pause", () => socket.broadcast.emit("movie-pause"));
-  socket.on("movie-seek", time => socket.broadcast.emit("movie-seek", time));
+  // -------- Movie sync --------
+  socket.on("movie-play", () =>
+    socket.to(ROOM_ID).emit("movie-play")
+  );
+
+  socket.on("movie-pause", () =>
+    socket.to(ROOM_ID).emit("movie-pause")
+  );
+
+  socket.on("movie-seek", time =>
+    socket.to(ROOM_ID).emit("movie-seek", time)
+  );
 
   socket.on("disconnect", () => {
-    userCount--;
-    console.log("User disconnected");
+    console.log("User disconnected:", socket.id);
+    const room = io.sockets.adapter.rooms.get(ROOM_ID);
+    const count = room ? room.size : 0;
+    io.to(ROOM_ID).emit("user-count", count);
   });
 });
 
-server.listen(port, () => {
-  console.log(`Server + Socket.IO running on port ${port}`);
-});
+server.listen(port, () =>
+  console.log(`Server running on port ${port}`)
+);
